736b5766808c83aae15e75b4d24cc5e864bb277b
Deepak Kumar <deepakkumarnd@gmail.com>
2025-07-12 23:08:13 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+
+class SnakeBoard(BaseModel):
+    grid: List[List[int]]
+
+
+class NextMoveResponse(BaseModel):
+    direction: int
+
+
+@app.get("/")
+def root_path():
+    return "ok"
+
+
+@app.post("/snakes/next-move",
+          response_model=NextMoveResponse,
+          summary='Predict the next move',
+          description='Accept the current state and predict the next move for the snake')
+def get_move(current_state: SnakeBoard):
+    grid = current_state.grid
+    print(grid)
+    return {"direction": random.randint(1, 4)}
