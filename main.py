750b45113582cf91c6f4513fc756f363b48a53d7
Deepak Kumar <deepakkumarnd@gmail.com>
2025-08-06 11:00:22 +0530

    Fix errors

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import random
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from snake_player import Agent
 
 app = FastAPI()
+agent = Agent()
 
 
 class SnakeBoard(BaseModel):
     grid: List[List[int]]
+
+
+class GameFeedback(BaseModel):
+    previous_grid: List[List[int]]
+    current_grid: List[List[int]]
+    reward: int
+    game_over: bool
+    move: int
 
 
 class NextMoveResponse(BaseModel):
@@ -26,11 +34,15 @@ def root_path():
           description='Accept the current state and predict the next move for the snake')
 def get_move(current_state: SnakeBoard):
     grid = current_state.grid
-    print(grid)
-    return {"direction": random.randint(1, 4)}
+    action = agent.predict(grid)
+    move = action + 1  # front end users 1,2,3,4 as moves
+    return {"direction": move}
 
 
 @app.post("/snakes/feedback")
-def feedback(current_state: SnakeBoard, reward: int = 0):
-    grid = current_state.grid
+def feedback(feedback_data: GameFeedback):
+    state = feedback_data.previous_grid
+    next_state = feedback_data.current_grid
+    action = feedback_data.move - 1
+    agent.feedback(state, action, feedback_data.reward, next_state, feedback_data.game_over)
     return {}
---
+++ snake_player.py
@@ -0,0 +1,92 @@
+import random
+import numpy as np
+import torch
+from torch import nn, optim
+
+
+class SnakeBoardStates:
+    def __init__(self):
+        self.states = []
+
+    def add(self, state, action, reward, next_state, game_over):
+        self.states.append((state, action, reward, next_state, game_over))
+
+    def sample(self, sample_size: int):
+        return random.sample(self.states, sample_size)
+
+    def __len__(self):
+        return len(self.states)
+
+
+class SnakePlayerModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        input_layer = nn.Linear(64, 512)
+        hidden_layer = nn.Linear(512, 512)
+        output_layer = nn.Linear(512, 4)
+        self.network = nn.Sequential(
+            input_layer,
+            nn.ReLU(),
+            hidden_layer,
+            nn.ReLU(),
+            output_layer
+        )
+
+    def forward(self, x):
+        return self.network(x)
+
+
+class Agent:
+    def __init__(self):
+        print("Agent initialized")
+        self.batch_size = 16
+        self.model = SnakePlayerModel()
+        self.model.eval()
+        self.policy_model = SnakePlayerModel()
+        self.policy_model.train()
+        self.history = SnakeBoardStates()
+        self.gamma = 0.6
+        self.loss_f = nn.MSELoss()
+        self.optimizer = optim.Adam(self.policy_model.parameters(), lr=0.1)
+
+    def predict(self, grid) -> int:
+        x = self._grid_to_vector(grid)
+        output = self.model(x).squeeze(1)
+        action = torch.argmax(output).item()
+        return action
+
+    def feedback(self, grid, action, reward, next_grid, game_over):
+        print("Move =", action, "Reward =", reward, "game over=", game_over)
+        current_state = self._grid_to_vector(grid)
+        next_state = self._grid_to_vector(next_grid)
+        self.history.add(current_state, action, reward, next_state, game_over)
+
+        if (len(self.history) % self.batch_size) == 0:
+            print("Start training history len=", len(self.history))
+            self.train()
+
+    def train(self):
+        for _ in range(10):
+            states, actions, rewards, next_states, game_over = zip(*self.history.sample(self.batch_size))
+            states = torch.cat(states)
+            next_states = torch.cat(next_states)
+            dones = (torch.tensor(game_over) * 1).unsqueeze(1)
+            outputs = self.policy_model(states)
+            # predicted Q values for the actions
+            q_values = torch.gather(outputs, dim=1, index=torch.tensor(actions).unsqueeze(1))
+            # predicted Q values for the next_states
+            values, _indices = self.policy_model(next_states).max(dim=1)
+            max_q_values = values.unsqueeze(1)
+            fut_q_values = torch.tensor(rewards).unsqueeze(1) + self.gamma * max_q_values * (1 - dones)
+            loss = self.loss_f(q_values.squeeze(1), fut_q_values.squeeze(1))
+            loss.backward()
+            self.optimizer.step()
+
+        self.model.load_state_dict(self.policy_model.state_dict())
+
+
+
+
+    def _grid_to_vector(self, grid):
+        flat_array = np.array(grid).flatten()
+        return torch.tensor(flat_array).float().unsqueeze(dim=0)
